74b838b8431c51f7bac9caa30166a63c76d9093c
ilml <mytomlong@gmail.com>
2023-08-23 00:52:21 -0700

    fix prompt

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 import openai
-openai.api_key = 
+openai.api_key = "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"
 MODEL = "text-davinci-003"
 MAX_TOKENS = 2000
 
@@ -10,6 +10,7 @@ PROMPT_PATH = "./data/prompt/"
 OJ_PATH = "./data/oj/"
 MEM_PATH = "./data/mem/"
 
-PREFIX = "### Instruction: You are a helpful AI Assistant. Please provide python solution based on the user's instructions, please only return python code. ### Input: "
+PREFIX = "### Instruction: You are a helpful AI Assistant. Please provide python solution based on the user's instructions,  \\\
+         please only return python code. Please consider optimized time and space complexity.  ### Input: "
 PROMPT = "Return the solution using the following class definition:\n"
 RESPONSE = "### Response:"
